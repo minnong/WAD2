ed3ab6461529b2f1a50adb8def61ea48dad09257
minnong <ongminn@gmail.com>
2025-09-17 00:13:48 +0800

    Initial commit for React with TailwindCSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-slate-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        React + TypeScript + Tailwind
+      </h1>
+    </div>
+  );
+}
